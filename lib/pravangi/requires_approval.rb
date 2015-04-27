265d8825f44da90f1c322e49171b878120409328
Jasdeep Singh <narang.jasdeep@gmail.com>
2015-04-27 02:03:01 -0400

    can commit pending approvals

--- lib/pravangi/requires_approval.rb
+++ lib/pravangi/requires_approval.rb
@@ -15,9 +15,18 @@ module Pravangi
         class_attribute :pravangi_options
         self.pravangi_options = options.dup
 
-        before_update :track_approval, if: options[:if]
+        attr_accessor :skip_approval
+
+        class_eval do 
+          def should_track_approval?
+            self.send(self.class.pravangi_options[:if]) && !skip_approval
+          end
+        end
+
+        before_update :track_approval, if: :should_track_approval?
 
         has_many :pending_approvals, 
+          lambda { order('id ASC') },
           class_name: 'Pravangi::PendingApproval',
           as: :resource
       end
@@ -27,15 +36,30 @@ module Pravangi
     def track_approval
       warn('Pravangi: The record cannot be updated, because it requires approval.')
       if changed?
-        pending_approvals.build(
-          object_changes: self.changes,
-          raw_object: self.reload.to_yaml
+        original = self.clone
+        original.pending_approvals.build(
+          object_changes: original.changes,
+          raw_object: original.to_yaml
           ).save
+        self.reload
       end
     end
 
     def pending_approval?
       pending_approvals.present?
+    end
+
+    def as_object
+      @object ||= YAML.load(raw_object)
+    end
+
+    def commit
+      resource.skip_approval = true
+      object_changes.each do |k,v|
+        resource[k] = v[1]
+      end
+      resource.save
+      resource.reload
     end
 
   end
--- spec/lib/pravangi_spec.rb
+++ spec/lib/pravangi_spec.rb
@@ -21,7 +21,7 @@ describe Pravangi do
     end
 
     class Post < ActiveRecord::Base
-      requires_approval if: :approved?
+      requires_approval if: :approved?, enabled: true
 
       def approved?
         true
@@ -30,15 +30,15 @@ describe Pravangi do
 
   end
 
-  context 'new record' do
+  context 'when creating a new' do
 
     let(:post) { Post.create(title: 'metaware') }
 
-    it 'should create a new record without any approval' do
+    it 'record gets created without the need for approval' do
       expect(post.title).to eq('metaware')
     end
 
-    it 'should not have any pending approvals' do
+    it 'record there are no pending approvals required' do
       expect(post.pending_approvals).to be_empty
     end
 
@@ -52,37 +52,37 @@ describe Pravangi do
       post.title = 'new metaware'
     end
 
-    context 'requires approval' do
+    context 'that requires approval' do
 
       before(:each) do
         post.save
         post.reload
       end
 
-      it 'doesnt update or change the record that requires approval' do
+      it 'should not update or change after saving' do
         expect(post.title).to eq('metaware')
       end
 
-      it 'saves a single record that contains pending changes' do
+      it 'should prepare a trail of pending approvals' do
         expect(post.title).to eq('metaware')
         expect(post.pending_approvals).to be_present
       end
 
-      it 'should return true when approval is required' do
+      it 'should allow to enquire if there are pending approvals' do
         expect(post.pending_approval?).to eq(true)
       end
 
-      context 'object_changes' do
+      context 'tracks object_changes' do
         
-        it 'should populate object_changes column' do
+        it 'should populate the object_changes column' do
           expect(post.pending_approvals.last.object_changes).to be_present
         end
 
-        it 'should be of type hash' do
+        it 'should verify the object_changes are stored in a Hash' do
           expect(post.pending_approvals.last.object_changes).to be_a(Hash)
         end
 
-        it 'should contain the attributes that changed' do
+        it 'should be possible to find out what attributes changed' do
           expect(post.pending_approvals.last.object_changes).to include('title', 'updated_at')
         end
 
@@ -90,12 +90,11 @@ describe Pravangi do
 
       context 'raw_object' do
         
-        it 'should populate serialized_object column' do
+        it 'should populate raw_object column' do
           expect(post.pending_approvals.last.raw_object).to be_present
         end
 
-
-        context 'serialized raw object' do
+        context 'serialized' do
 
           let(:serialized_object) { YAML.load(post.pending_approvals.last.raw_object) }
 
@@ -104,13 +103,70 @@ describe Pravangi do
           end
 
           it 'should have the correct (new) attributes' do
-            expect(serialized_object.title).to eq('metaware')
+            expect(serialized_object.title).to eq('new metaware')
           end
 
         end
 
       end
       
+    end
+
+    context 'multiple changes' do
+
+      before(:each) do
+        post.title = 'metaware 2'
+        post.save
+        post.title = 'metaware 3'
+        post.save
+        post.reload
+      end
+
+      it 'should recognize 2 pending approvals' do
+        expect(post.pending_approvals.count).to eq(2)
+      end
+
+      it 'should recognize approvals in order' do
+        skip
+        # expect(post.pending_approvals.first).to eq(2)
+      end
+
+    end
+
+    context 'deserialized object' do
+
+      before(:each) do
+        post.title = 'metaware 2'
+        post.save
+        post.title = 'metaware 3'
+        post.save
+        post.reload
+      end
+
+      it 'should have the anticipated changes' do
+        first = post.pending_approvals.first.as_object
+        second = post.pending_approvals.last.as_object
+
+        expect(first.title).to eq('metaware 2')
+        expect(second.title).to eq('metaware 3')
+      end
+
+    end
+
+    context '#commit' do
+
+      before(:each) do
+        post.title = 'metaware 2'
+        post.save
+        post.reload
+      end
+
+      it 'should be able to revert the object back to the requested state' do
+        post.pending_approvals.last.commit
+        post.reload
+        expect(post.title).to eq('metaware 2')
+      end
+
     end
 
     context 'does not requires approval' do
